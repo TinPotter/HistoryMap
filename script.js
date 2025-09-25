496c4f4c45882b011c811734715373938ea4fff7
a complete idiot👽 <115880460+TinPotter@users.noreply.github.com>
2025-09-25 20:28:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// script.js
+// Create a map centered on Paris
+var map = L.map('map').setView([48.8566, 2.3522], 5);
 
-// Initialize map, centered on Europe
-var map = L.map('map').setView([48.8566, 2.3522], 5); // Paris coords
-
-// Add OpenStreetMap tiles
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© OpenStreetMap contributors',
+// Dark Matter tiles from Carto
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/">OSM</a> &copy; <a href="https://carto.com/">CARTO</a>',
+  subdomains: 'abcd',
+  maxZoom: 19
 }).addTo(map);
 
 // Add a draggable marker
-var marker = L.marker([48.8566, 2.3522], {draggable: true}).addTo(map);
-marker.bindPopup("Drag me around!").openPopup();
+var marker = L.marker([48.8566, 2.3522], { draggable: true }).addTo(map);
+marker.bindPopup("Drag me!").openPopup();
